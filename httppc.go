374accb10c96674c5cc38471b5019ce05254ed91
vislee <louivislee@gmail.com>
2018-11-11 10:43:59 +0800

    add files

---
+++ httppc.go
@@ -0,0 +1,130 @@
+// Copyright (C) vislee
+
+package httppc
+
+import (
+	"bytes"
+	"context"
+	"log"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+)
+
+type proxyProClient struct {
+	http.Client
+}
+
+type proxyProDialer struct {
+	net.Dialer
+	cliAddr string
+	serAddr string
+}
+
+func (self *proxyProDialer) DialContext(ctx context.Context, network, address string) (net.Conn, error) {
+	conn, err := self.Dialer.DialContext(ctx, network, address)
+
+	if err != nil {
+		log.Println(err.Error())
+		return conn, err
+	}
+
+	var (
+		protol  string
+		cliAddr string
+		cliPort string
+		serAddr string
+		serPort string
+	)
+
+	lAddr := conn.LocalAddr()
+	raddr := conn.RemoteAddr()
+
+	protol, serAddr, serPort = parseAddr(raddr.Network(), raddr.String())
+	_, cliAddr, cliPort = parseAddr(lAddr.Network(), lAddr.String())
+
+	if self.serAddr != "" {
+		serAddr = self.serAddr
+	}
+	if self.cliAddr != "" {
+		cliAddr = self.cliAddr
+	}
+
+	ppBuff := bytes.NewBuffer([]byte("PROXY "))
+
+	ppBuff.WriteString(protol)
+	ppBuff.WriteString(" ")
+
+	ppBuff.WriteString(cliAddr)
+	ppBuff.WriteString(" ")
+
+	ppBuff.WriteString(serAddr)
+	ppBuff.WriteString(" ")
+
+	ppBuff.WriteString(cliPort)
+	ppBuff.WriteString(" ")
+
+	ppBuff.WriteString(serPort)
+	ppBuff.WriteString("\r\n")
+
+	_, err = conn.Write(ppBuff.Bytes())
+	if err != nil {
+		conn.Close()
+	}
+
+	return conn, err
+}
+
+var ppDialer = &proxyProDialer{
+	Dialer: net.Dialer{Timeout: 30 * time.Second, KeepAlive: 30 * time.Second, DualStack: true},
+}
+
+var ppTransport http.RoundTripper = &http.Transport{
+	Proxy:                 http.ProxyFromEnvironment,
+	DialContext:           ppDialer.DialContext,
+	MaxIdleConns:          100,
+	IdleConnTimeout:       90 * time.Second,
+	TLSHandshakeTimeout:   10 * time.Second,
+	ExpectContinueTimeout: 1 * time.Second,
+}
+
+func NewProxyProClient() *proxyProClient {
+	pc := &proxyProClient{
+		http.Client{
+			Transport: ppTransport,
+			Timeout:   90 * time.Second,
+		},
+	}
+	return pc
+}
+
+func (self *proxyProClient) SetProxyProClientIP(remoteAddr string) {
+	ppDialer.cliAddr = remoteAddr
+}
+
+func (self *proxyProClient) SetProxyProServerIP(serAddr string) {
+	ppDialer.serAddr = serAddr
+}
+
+func parseAddr(net, addr string) (string, string, string) {
+	if net == "" {
+		return "UNKNOWN", "", ""
+	}
+
+	// log.Println(addr)
+
+	colon := strings.IndexByte(addr, ':')
+	if colon == -1 {
+		return strings.ToUpper(net) + "4", addr, "80"
+	}
+
+	if i := strings.Index(addr, "]:"); i != -1 {
+		return strings.ToUpper(net) + "6", addr[1:i], addr[i+len("]:"):]
+	}
+	if strings.Contains(addr, "]") {
+		return strings.ToUpper(net) + "6", addr[1 : len(addr)-2], "80"
+	}
+
+	return strings.ToUpper(net) + "4", addr[:colon], addr[colon+len(":"):]
+}
---
+++ httppc_test.go
@@ -0,0 +1,108 @@
+// Copyright (C) vislee
+
+package httppc
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestProxyAddrV4(t *testing.T) {
+	pp, addr, port := parseAddr("TCP", "127.0.0.1:8080")
+	if pp != "TCP4" {
+		t.Error("parseAddr protol error")
+	}
+
+	if addr != "127.0.0.1" {
+		t.Error("parseAddr addr error")
+	}
+
+	if port != "8080" {
+		t.Error("parseAddr port error")
+	}
+}
+
+func TestProxyAddrV6(t *testing.T) {
+	pp, addr, port := parseAddr("TCP", "[ffff::ffff]:8080")
+	if pp != "TCP6" {
+		t.Error("parseAddr ipV6 protol error")
+	}
+
+	if addr != "ffff::ffff" {
+		t.Error("parseAddr ipV6 addr error")
+	}
+
+	if port != "8080" {
+		t.Error("parseAddr ipV6 port error")
+	}
+}
+
+func TestProxyProClient(t *testing.T) {
+	ch := make(chan string, 1)
+
+	clientAddr := "1.1.1.2"
+	serverAddr := "2.2.2.3"
+	hostport := "127.0.0.1:8989"
+	url := fmt.Sprintf("http://%s/", hostport)
+
+	ln, err := net.Listen("tcp", hostport)
+	if err != nil {
+		t.Error(err.Error())
+	}
+
+	go func() {
+		conn, err := ln.Accept()
+		if err != nil {
+			t.Error(err.Error())
+		}
+
+		b := make([]byte, 1024)
+		conn.Read(b)
+		ch <- string(b)
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: 2\r\nConnection: close\r\n\r\nOK"))
+		conn.Close()
+	}()
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Error("new request error", err.Error())
+	}
+	req.Host = "www.test.com"
+	req.Header.Add("X-test-waf", "test-waf")
+
+	pc := NewProxyProClient()
+	pc.SetProxyProClientIP(clientAddr)
+	pc.SetProxyProServerIP(serverAddr)
+
+	resp, err := pc.Do(req)
+	if err != nil {
+		t.Error("Do error", err.Error())
+	}
+
+	if resp.StatusCode != 200 {
+		t.Error("resp StatusCode error. ", resp.StatusCode)
+	}
+
+	buf := make([]byte, 2)
+	n, err := resp.Body.Read(buf)
+	if n == 0 && err != nil {
+		t.Error(err.Error())
+	}
+
+	if n != 2 || string(buf) != "OK" {
+		t.Error("body error", n)
+	}
+	resp.Body.Close()
+
+	bb := <-ch
+	if !strings.HasPrefix(bb, fmt.Sprintf("PROXY TCP4 %s %s", clientAddr, serverAddr)) {
+		t.Error("proxy protol error.", bb)
+	}
+
+	ln.Close()
+	close(ch)
+}
+
